50a7b1b30e82dc5c43ce60902feb38ae1e63b433
omarquezada1 <64980731+omarquezada1@users.noreply.github.com>
2020-05-18 11:17:18 -0500

    Seguimos arreglando errores

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -4,13 +4,13 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class ProductoService {
-  URL_API = 'http://localhost:3000/producto';
+  URL_API = 'http://localhost:3000/api/producto';
 
   constructor(private http: HttpClient) { }
   // aqui es donde manda a llamar al back 
  postproducto(pro): Promise<any> {
   return new Promise((resolve, reject) => {
-    this.http.post(this.URL_API, pro ).subscribe(res => {
+    this.http.post(`${this.URL_API}/registrar`, pro ).subscribe(res => {
       console.log('respuesta', res);
       resolve();
     }, err => {
