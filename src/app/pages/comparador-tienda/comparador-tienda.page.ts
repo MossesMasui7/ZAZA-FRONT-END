48dec93442d51c11d73a8caa6498e6a2cc77820d
ivanperez1212 <ivanperezjimenez1l40@outlook.es>
2020-07-30 18:57:41 -0500

    esesta

--- src/app/pages/comparador-tienda/comparador-tienda.page.ts
+++ src/app/pages/comparador-tienda/comparador-tienda.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { NegocioService } from "../../services/negocio.service";
 import { RegistroService } from "../../services/usuario.service";
 import { MyserviceService } from "../../services/myservice.service";
+
+import { Console } from 'console';
+import { ProductoService } from '../../services/producto.service';
 @Component({
   selector: 'app-comparador-tienda',
   templateUrl: './comparador-tienda.page.html',
@@ -14,28 +17,85 @@ export class ComparadorTiendaPage implements OnInit {
   public data;
   idNegocio: String;
   idNegocio2: String;
+  productosPrecio:any[] =[]
+  negocio: any[]=[]
   constructor( public negocios: NegocioService,private usuarioService: RegistroService,
-    private usuario: MyserviceService) { }
+    private usuario: MyserviceService, private productoService: ProductoService) { }
 
   ngOnInit() {
+    this.negociosCarrito()
     this.negocios.obtenerNegocios().subscribe((data) => {
       this.negociosCercanos = data;
     });
     if (this.usuario.usuario["carrito"].length > 0) {
       this.mostrarCarrito = true;
+      
       this.mostrarVacio = false;
     } else {
       this.mostrarCarrito = false;
       this.mostrarVacio = true;
     }
-     
+  
+  } 
+
+  // precio1(idNegocio){
+  //   this.productoService.buscarprecio().subscribe(data => {
+      
+  //   })
+
+
+  // }
+
+onChange($event){
+  let producto = []
+  producto = []
+  this.productoService.buscarprecio($event.target.value).then((data)=>{
+    data['resp'].forEach(db => {
+      this.usuario.usuario['carrito'].forEach(carrito => {
+        if (db['Producto'] == carrito['producto']['_id']) {
+          producto.push({
+            "Producto":carrito['producto']['descripcion'],
+            "Precio": db['Precio']
+          })
+         
+          
+        }else{
+          producto.push({
+            "Producto":carrito['producto']['descripcion'],
+            "Precio": '--'
+          })
+        }
+      });
+    });
+    this.productosPrecio = producto
+   
+    
+  }).catch((err)=>{
+    console.log(err);
+    
+  })
+}
+
+  negociosCarrito(){
+let negocios =[]
+    this.usuario.usuario['carrito'].forEach(element => {
+      
+      negocios.push({
+        'nombre':element['negocio']['nombre'],
+        '_id':element['negocio']['_id']
+      })
+      
+      
+    });
+
+    this.negocio = negocios
+
+    
     
   }
-  eliminar(id: String) {
-    this.usuarioService.eliminarCarrito(id).then((data) => {
-      this.usuario.usuario = data["cont"];
-    });
-  }
+
+
+ 
  
 
 }
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { MyserviceService } from './myservice.service';
 @Injectable({
   providedIn: "root",
 })
 export class ProductoService {
-  URL_API = "https://zaza-app.herokuapp.com/api/producto";
+  //URL_API = "https://zaza-app.herokuapp.com/api/producto";
   //URL_API = "http://192.168.1.79:3000/api/producto";
+    URL_API = "http://localhost:3000/api";
+
   public precio = [];
   public tiendas = {};
   public productos: any[] = [];
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,public usuario:MyserviceService) {}
 
   // aqui es donde manda a llamar al back
   postproducto(pro): Promise<any> {
@@ -55,5 +58,8 @@ export class ProductoService {
   }
   obtenerComentarios(id: any){
     return this.http.get(`${this.URL_API}/producto/obtenerComentarios/${id}`).toPromise();
+  }
+  buscarprecio(idNegocio){
+    return this.http.get(`${this.URL_API}/producto/comparar/${this.usuario.usuario['_id']}/${idNegocio}`).toPromise()
   }
 }
